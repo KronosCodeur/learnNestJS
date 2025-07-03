181120b6c248b940e3f992e3b3cd96ce2be7d10f
KronosCodeur <piraliarris@gmail.com>
2025-07-03 09:41:23 +0000

    Add Todo, Auth, Users modules and configure environment setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import {TodoModule} from "./todo/todo.module";
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import {ConfigModule} from "@nestjs/config";
 
 @Module({
-  imports: [],
+  imports: [TodoModule, AuthModule, UsersModule, ConfigModule.forRoot({
+    envFilePath: [
+        '.env.dev',
+        '.env'
+    ],
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
